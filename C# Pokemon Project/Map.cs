66736b0cc0020ca7638ea0ae89e1076572d90e1b
Daviddeoliveira <ddeoliveira@gaming.tech>
2024-02-06 11:56:40 +0100

    nouvelle carte affiche mais a fix sur les deplacement du joueur

--- C# Pokemon Project/Map.cs
+++ C# Pokemon Project/Map.cs
@@ -1,42 +1,34 @@
 using System;
+using System.IO;
 class Map
 {
     public char[,] map;
-    public int size_x = 20;
-    public int size_y = 60;
+    public int size_x;
+    public int size_y;
     public Npc npc = new Npc();
+
 
     public Map()
     {
+        string[] lines = File.ReadAllLines("data/carte.txt");
+        size_x = lines.Length;
+        size_y = lines[0].Length;
+
+        for (int i = 1; i < size_x; i++)
+        {
+            if (lines[i].Length != size_y)
+            {
+                throw new ArgumentException("Les lignes du fichier ne sont pas de la même longueur.");
+            }
+        }
+
         map = new char[size_x, size_y];
+
         for (int x = 0; x < size_x; x++)
         {
             for (int y = 0; y < size_y; y++)
             {
-                if (x == 0 && y == 0 || x == 0 && y == size_y - 1)
-                {
-                    map[x, y] = ' ';
-                }
-                else if (y == 0 || y == size_y - 1)
-                {
-                    map[x, y] = '|';
-                }
-                else if (x == 0 || x == size_x - 1)
-                {
-                    map[x, y] = '_';
-                }
-                else if((x >= 10 && x <= 13 && y >= 10 && y <= 13) || (x >= 1 && x <= 6 && y >= 40 && y <= 48) || (x >= 12 && x <= 14 && y >= 28 && y <= 34) || (x >= 1 && x <= 4 && y >= 15 && y <= 17) || (x >= 3 && x <= 4 && y >= 18 && y <= 19))
-                {
-                    map[x, y] = '"';
-                }
-                else if((x == 1 && y == 35))
-                {
-                    map[x, y] = 'O';
-                }
-                else
-                {
-                    map[x, y] = ' ';
-                }
+                map[x, y] = lines[x][y];
             }
         }
     }
