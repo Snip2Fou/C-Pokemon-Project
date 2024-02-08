556b61c93bde2b7757941a4354de2b3d378ce453
Daviddeoliveira <ddeoliveira@gaming.tech>
2024-02-08 20:50:39 +0100

    moyenne des niveaux

--- C# Pokemon Project/Game.cs
+++ C# Pokemon Project/Game.cs
@@ -446,7 +446,14 @@ public class Game
                     int random_pokemon = random.Next(1, 1061);
                     // Combat entre les deux dresseurs
 
-                    Battle battle = new Battle(); 
+                    Battle battle = new Battle();
+                    int sommeNiveauPokemonPlayer = 0;
+                    foreach (var poke in player.Team)
+                    {
+                        sommeNiveauPokemonPlayer += poke.Level;
+                    }
+                    int moyenneNiveauxPokemonPlayer = sommeNiveauPokemonPlayer / (player.Team.Count + 1);
+                    pokemons[random_pokemon].Level = moyenneNiveauxPokemonPlayer;
                     battle.StartBattleVsPokemon(player, pokemons[random_pokemon]);
                   
                     if (!player.TeamIsAlive())
